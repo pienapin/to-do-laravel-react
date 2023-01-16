c04da913ce7bd962ca3288c4aa6d3358ba4334d6
Alvien Cenna <pincenna@gmail.com>
2023-01-16 20:26:24 +0700

    add layouts for guest and authorized user

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,4 +1,7 @@
-import {createBrowserRouter} from 'react-router-dom';
+import {createBrowserRouter, Navigate} from 'react-router-dom';
+import DefaultLayout from './components/DefaultLayout';
+import GuestLayout from './components/GuestLayout';
+import Dashboard from './views/Dashboard';
 import Login from './views/Login';
 import NotFound from './views/NotFound';
 import Signup from './views/Signup';
@@ -6,16 +9,38 @@ import Users from './views/Users';
 
 const router = createBrowserRouter([
   {
-    path: '/login',
-    element: <Login/>,
+    path: '/',
+    element: <DefaultLayout/>,
+    children:
+    [
+      {
+        path: '/',
+        element: <Navigate to="/dashboard"/>
+      },
+      {
+        path: '/users',
+        element: <Users/>,
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard/>,
+      }
+    ],
   },
   {
-    path: '/signup',
-    element: <Signup/>,
-  },
-  {
-    path: '/users',
-    element: <Users/>,
+    path: '/',
+    element: <GuestLayout/>,
+    children:
+    [
+      {
+        path: '/login',
+        element: <Login/>,
+      },
+      {
+        path: '/signup',
+        element: <Signup/>,
+      },
+    ]
   },
   {
     path: '*',
